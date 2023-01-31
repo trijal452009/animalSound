e759bf58a5713219ffb390cb37f5c4a7bf922cb6
trijal452009 <89978519+trijal452009@users.noreply.github.com>
2023-01-31 19:05:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/GqNKRz3gX/model.json', modelReady);
+
+}
+
+ function modelReady(){
+    classifier.classify(gotResults);
+ }
+ var dog=0;
+ var cat=0;
+ var cow=0;
+ var lion=0;
+
+ function gotResults(error, results){
+    if (error){
+        console.error(error);
+
+    }
+    else{
+        console.log(results);
+         random_number_r= Math.floor(Math.random()*255)+1;
+         random_number_g= Math.floor(Math.random()*255)+1;
+         random_number_b= Math.floor(Math.random()*255)+1;
+
+         document.getElementById("result_label").innerHTML='I can hear- '+ results[0].label;
+         document.getElementById("result_confidence").innerHTML='accuracy-'+ (results[0].confidence*100).toFixed(2)+"%";
+         document.getElementById("result_label").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+         document.getElementById("result_confidence").style.color="rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+
+
+
+         
+         if(results[0].label=="cow's moo"){
+            img.src='cow_moo.gif';
+           cow=cow+1;
+
+         }
+
+         else if(results[0].label=="dog's bark"){
+            img.src='dog_barking.gif';
+            dog=dog+1;
+
+         }
+         else if(results[0].label=="meow"){
+            img.src='cat_meowing.gif';
+            cat=cat+1;
+   
+    }
+    else if(results[0].label=="roar"){
+      img.src='lion-roar.gif';
+            cat=cat+1;
+    }
+   
+    else{
+        
+            
+            img.src='hearing_gif.gif';
+    }
+
+    }
+
+ }
